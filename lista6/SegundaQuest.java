8c618b4bafa28e8c0914e4163e59e7a79468b425
JonnyKinCoronetti <joao.vc2002@aluno.ifsc.edu.br>
2025-10-17 00:33:17 +0000

    quest 2 clomplete

---
+++ lista6/SegundaQuest.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class SegundaQuest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Leitura dos três lados
+        System.out.print("Digite o lado a: ");
+        double a = scanner.nextDouble();
+        System.out.print("Digite o lado b: ");
+        double b = scanner.nextDouble();
+        System.out.print("Digite o lado c: ");
+        double c = scanner.nextDouble();
+
+
+        if (a + b > c && b + c > a && c + a > b) {
+
+            if (a == b && b == c) {
+                System.out.println("Triângulo Equilátero");
+            } else if (a == b || b == c || a == c) {
+                System.out.println("Triângulo Isósceles");
+            } else {
+                System.out.println("Triângulo Escaleno");
+            }
+        } else {
+            System.out.println("Os lados fornecidos NÃO formam um triângulo válido.");
+        }
+
+        scanner.close();
+    }
+}
+
+
